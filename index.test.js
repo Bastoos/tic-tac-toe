c714b7fd3f0f438da7115d23ba1c324a86413f3f
Venâncio Bastos <vjpb2002@gmail.com>
2024-12-04 13:45:15 +0000

    fixing test implementations

--- index.test.js
+++ index.test.js
@@ -1,5 +1,30 @@
 const checkWinner = require("./index");
 
-test("The player one should win!", () => {
-  expect(checkWinner([1, 2, 3])).toBe("win");
+test("Player wins with a horizontal line", () => {
+  const playerPositions = [1, 2, 3];
+  expect(checkWinner(playerPositions)).toBe("win");
+});
+
+test("Player wins with a vertical line", () => {
+  const playerPositions = [1, 4, 7];
+  expect(checkWinner(playerPositions)).toBe("win");
+});
+
+test("Player wins with a diagonal line", () => {
+  const playerPositions = [1, 5, 9];
+  expect(checkWinner(playerPositions)).toBe("win");
+});
+
+test("Game is still pending without any winning positions", () => {
+  const playerPositions = [1, 2];
+  expect(checkWinner(playerPositions)).toBe("pending");
+});
+
+test("No positions selected yet", () => {
+  expect(checkWinner([])).toBe("pending");
+});
+
+test("Random positions that do not lead to a win", () => {
+  const playerPositions = [1, 2, 4];
+  expect(checkWinner(playerPositions)).toBe("pending");
 });
